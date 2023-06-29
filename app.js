661523707ff317d8ab8ab678ae19f3ba1c722fe5
yanyanyaa <yclin3411@gmail.com>
2023-06-29 15:27:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
 const exhbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })  
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
