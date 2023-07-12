3d94fe940b4de6237b3198b2687abd326268dc7b
yanyanyaa <yclin3411@gmail.com>
2023-07-13 01:50:16 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const session = require('express-session')
 const exhbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const routes = require('./routes')
 require('./config/mongoose')
 
 const usePassport = require('./config/passport')
 
-const port = 3000
+const PORT = process.env.PORT
 
 
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret', 
+  secret: process.env.SESSION_SECRET, 
   resave: false,
   saveUninitialized: true
 }))
@@ -37,6 +40,6 @@ app.use((req, res, next) => {
 })
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`)
 })
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home.js')
 const todos = require('./modules/todos.js')
 const users = require('./modules/users.js')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth.js')
 
-router.use('/todos',  authenticator, todos)
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
